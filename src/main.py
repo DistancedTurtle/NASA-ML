fixed to_parquet to correctly numerically represent bools and strings using one-hot encoding for strings.

src/main.py
@@ -11,5 +11,11 @@ import torch.optim as optim
 from pathlib import Path
 import json
 
-df = pd.read_json(f'{Path.cwd()}/data/near_earth_objects.json')
-df.info()
+
+file_path = Path.cwd() / "data" / "neos_ml.parquet"
+df = pd.read_parquet(file_path)
+pd.set_option('display.max_columns', None)
+print(df.head())
+all_values = df.values
+tensor_data = torch.tensor(all_values, dtype = torch.float32)
+print(tensor_data[0,:])